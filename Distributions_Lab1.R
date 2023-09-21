c646fde35cf49c5070d6881b27fbc564ecb83bdb
jannatul786 <jannat786@gmail.com>
2023-09-21 10:26:55 -0400

    Data Analytics Fall-2023 Lab_1

--- Distributions_Lab1.R
+++ Distributions_Lab1.R
@@ -1,5 +1,77 @@
 #Exercise 1: Fitting a distribution beyond histograms
 #---------------------------------------------------#
+setwd("D:/DIBA/ITWS_4600/Resources/EPI_Dataset")
+EPI_data_2010 <- read.csv("EPI_data_2010.csv")
+View(EPI_data_2010)
+
+attach(EPI_data_2010)
+names(EPI_data_2010) <- as.matrix(EPI_data_2010[1,])
+EPI_data_2010 <- EPI_data_2010[-1,] #change the first row to be the header in R
+View(EPI_data_2010)
+EPI_data_2010$EPI
 
 #Cumulative density function
+#~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#ecdf() allows to plot a feature of data in order from least to greatest 
+#and see the whole feature as if is distributed across the data set.
+
+#do.points: logical; if TRUE, also draw points at the (xlim restricted) knot locations. 
+#Default is true, for sample size <100
+#verticals: logical; if TRUE, draw vertical lines at steps.
+
+help(ecdf) #Empirical Cumulative Distribution Function
+plot(ecdf(EPI_data_2010$EPI), do.points = FALSE, verticals = TRUE)
+
+#Quantile-Quantile(Q-Q) Plot/probability plot
+#qqPlot allows the user to specify a number of different distributions 
+#in addition to the normal distribution, 
+#and to optionally estimate the distribution parameters of the fitted distribution
+help("par") #Set or Query Graphical Parameters
+par(pty = "s")
+#A character specifying the type of plot region to be used; 
+#"s" generates a square plotting region and 
+#"m" generates the maximal plotting region.
+
+help("qqnorm")
+help("qqplot")
+#qqnorm(EPI_data_2010$EPI);qqline(EPI_data_2010$EPI)
+qqnorm(EPI_data_2010$EPI)
+qqline(EPI_data_2010$EPI) #adding the line to Q-Q plot
+x <- seq(30,95,1)
+qqplot(qt(ppoints(250),df=5), x, xlab = "Q-Q plot for t-distribution") 
+#The function qt(x, df) returns the value of the inverse cumulative density function (cdf)
+#ppoints() is used in qqplot and qqnorm to generate the set of probabilities at which to evaluate the inverse distribution.
+qqline(x)
+
+#~~~~For DALY~~~~~~#
+plot(ecdf(EPI_data_2010$DALY), do.points = FALSE, verticals = TRUE)
+par(pty = "s")
+qqnorm(EPI_data_2010$DALY)
+qqline(EPI_data_2010$DALY)
+x <- seq(30,95,1)
+qqplot(qt(ppoints(250), df=5), x, xlab = "Q-Q plot for t-distribution")
+qqline(x)
+
+#~~~~For WATER_H~~~~~~#
+plot(ecdf(EPI_data_2010$WATER_H), do.points = FALSE, verticals = TRUE)
+qqnorm(EPI_data_2010$WATER_H)
+qqline(EPI_data_2010$WATER_H)
+x <- seq(30,95,1)
+qqplot(qt(ppoints(250), df=5), x, xlab = "Q-Q plot for t-distribution")
+qqline(x)
+
+#intercomparing the distributions
+qqplot(EPI_data_2010$EPI, EPI_data_2010$DALY)
+qqplot(EPI_data_2010$EPI, EPI_data_2010$ECOSYSTEM)
+qqplot(EPI_data_2010$EPI, EPI_data_2010$ENVHEALTH)
+qqplot(EPI_data_2010$EPI, EPI_data_2010$AIR_H)
+qqplot(EPI_data_2010$EPI, EPI_data_2010$WATER_H)
+qqplot(EPI_data_2010$EPI, EPI_data_2010$BIODIVERSITY)
+
+boxplot(EPI_data_2010$EPI, EPI_data_2010$DALY)
+boxplot(EPI_data_2010$EPI, EPI_data_2010$ECOSYSTEM)
+boxplot(EPI_data_2010$EPI, EPI_data_2010$ENVHEALTH)
+boxplot(EPI_data_2010$EPI, EPI_data_2010$AIR_H)
+boxplot(EPI_data_2010$EPI, EPI_data_2010$WATER_H)
+boxplot(EPI_data_2010$EPI, EPI_data_2010$BIODIVERSITY)
 
--- Summary_Lab1.R
+++ Summary_Lab1.R
@@ -12,7 +12,7 @@ EPI_data_2010 <- EPI_data_2010[-1,] #change the first row to be the header in R
 #or,
 EPI_data_2010[] <- lapply(EPI_data_2010, function(x){
   type.convert(as.character(x))
-})
+  })
 EPI_data_2010
 View(EPI_data_2010)
 EPI_data_2010$EPI #prints out values from data file which has column "EPI"
@@ -22,7 +22,13 @@ tf
 E <- EPI_data_2010$EPI[!tf] #filters out NA values , new array
 E
 summary(EPI_data_2010$EPI) #stats
+summary(EPI_data_2010$DALY)
+
 fivenum(EPI_data_2010$EPI, na.rm = TRUE)
+fivenum(EPI_data_2010$DALY, na.rm = TRUE)
+
+boxplot(EPI_data_2010$EPI, EPI_data_2010$DALY)
+
 stem(EPI_data_2010$EPI) 
 #Stem and Leaf plot is a technique of displaying the frequencies with which some classes of values may occur. 
 #output: records are arranged based on their most significant digit. The stem is the left side of the chart while the right side is the leaf.
@@ -31,6 +37,18 @@ hist(EPI_data_2010$EPI, seq(30.,95.,1.0), prob = TRUE)
 lines(density(EPI_data_2010$EPI, na.rm = TRUE, bw = 1.)) 
 #OR,
 lines(density(EPI_data_2010$EPI, na.rm = TRUE, bw = "SJ"))
+
+xn <- seq(30,95,1) #incremented by 1, like 31,32,......,94,95
+xn
+#Normal Distribution: dnorm(x, mean = 0, sd = 1, log = FALSE)
+#x: vector of quantiles.
+qn <- dnorm(xn, mean = 63, sd = 5, log = FALSE)
+help("dnorm")
+lines(xn,qn)
+lines(xn, .4*qn)
+ln <- dnorm(xn, mean = 44, sd = 5,log = FALSE)
+lines(xn, .26*ln)
+
 rug(EPI_data_2010$EPI)
 #A rug plot is a compact visualisation designed to supplement a 2d display with the two 1d marginal distributions.
 #Rug plots display individual cases so are best used with smaller datasets.
