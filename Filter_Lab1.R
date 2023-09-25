a87205961d8069e9e69c371f1c608c25dde4798e
jannatul786 <jannat786@gmail.com>
2023-09-25 17:03:22 -0400

    Data Analytics Fall-2023 Lab_1

--- Filter_Lab1.R
+++ Filter_Lab1.R
@@ -17,8 +17,19 @@ EPILand <- EPI_data_2010[!EPI_data_2010$Landlock, ]
 View(EPILand)
 Eland <- EPILand[!is.na(EPILand$Landlock), ]
 View(Eland)
+hist(Eland$No_surface_water, main = "Histogram of No_surface_water",
+     xlab = "No_surface_water", ylab = "Frequency",
+     col = "aquamarine1")
+hist(Eland$Desert, breaks = seq(0,1,0.1), prob = TRUE,
+     main = "Histogram of Desert with Probability Density",
+     xlab = "Desert", ylab = "Density",
+     col = "burlywood1")
+hist(Eland$High_Population_Density, breaks = seq(0,1,0.2), prob = TRUE,
+     main = "Histogram of High_Population_Density with Probability Density",
+     xlab = "High_Population_Density", ylab = "Density",
+     col = "chartreuse4")
 
-# Filtering based on specified conditions in EPI_regions
+# Filtering in EPI_regions(using specific condition)
 EPI_South_Asia <- EPI_data_2010[EPI_data_2010$EPI_regions == "South Asia", ]
 View(EPI_South_Asia)
 
